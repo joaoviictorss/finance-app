ea41fa644f4563f0686ec67298ae12b26ce0cfc9
João Victor dos Santos Silva <137708191+joaoviictorss@users.noreply.github.com>
2024-07-20 01:01:56 -0300

    feat: add resgister page

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,9 +1,52 @@
 import { SafeAreaView, Text, View } from "react-native";
+import CustomTextInput from "../components/ui/custom-text-input";
+import PasswordInput from "../components/ui/password-input";
+import Button from "../components/ui/button";
+import { Link } from "expo-router";
 
 const RegisterPage = () => {
   return (
-    <SafeAreaView>
-      <Text>Register page</Text>
+    <SafeAreaView className="flex-1 flex-col bg-customGreen-500">
+      <View className="items-center h-1/4 justify-center relative">
+        <Text className="text-customGreen-900 font-[PoppinsSemiBold] text-3xl">
+          Criar conta
+        </Text>
+      </View>
+
+      <View className="bg-customGreen-200 flex-1 rounded-t-[40px] p-8 flex-col gap-4 pt-14">
+        <CustomTextInput label="Nome" placeholder="Nome completo" />
+        <CustomTextInput label="Email" placeholder="exemplo@email.com" />
+        <CustomTextInput label="Data de nascimento" placeholder="DD/MM/AAAA" />
+        <PasswordInput label="Senha" />
+        <PasswordInput label="Confirmar senha" />
+
+        <View className="flex-col items-center mt-4">
+          <Text className="text-center text-customGreen-900 text-sm">
+            Ao continuar, você concorda com os
+          </Text>
+          <Text className="text-center text-customGreen-900 text-sm">
+            <Text className="font-[PoppinsSemiBold] text-sm">
+              Termos de uso
+            </Text>{" "}
+            e{" "}
+            <Text className="font-[PoppinsSemiBold] text-sm">
+              politica de privacidade
+            </Text>{" "}
+            .
+          </Text>
+          <Button variant="secondary" className="mt-5">
+            <Text className="text-customGreen-900 font-[PoppinsBold] text-lg">
+              Criar conta
+            </Text>
+          </Button>
+          <Text className="mt-4 font-[PoppinsLight] text-customGreen-900">
+            Ja possui uma conta?{" "}
+            <Link href={"/sign-in"} className="color-customBlue-500">
+              Entrar
+            </Link>
+          </Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,17 +1,19 @@
 import { cn } from "@/src/lib/util";
-import { Pressable, Text } from "react-native";
+import { Pressable } from "react-native";
 
 interface ButtonProps extends React.ComponentProps<typeof Pressable> {
   variant: "primary" | "secondary";
   children: React.ReactNode;
+  className?: string;
 }
 
-const Button = ({ variant, children, ...rest }: ButtonProps) => {
+const Button = ({ variant, children, className, ...rest }: ButtonProps) => {
   return (
     <Pressable
       className={cn(
         "rounded-[30px] items-center justify-center h-12 w-52",
-        variant === "primary" ? "bg-customGreen-300" : "bg-customGreen-500 "
+        variant === "primary" ? "bg-customGreen-300" : "bg-customGreen-500 ",
+        className
       )}
       {...rest}
     >
